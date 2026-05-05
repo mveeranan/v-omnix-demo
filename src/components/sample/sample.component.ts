b5c0cca68a8cc12ebc7c6f1fc841c80305e9018b
Veeranan M <veeranan.m@sword-group.com>
2026-05-05 16:03:18 +0400

    just added a form to chcek with validations

--- src/components/sample/sample.component.ts
+++ src/components/sample/sample.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { JsonPipe, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-sample',
   standalone: true,
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, NgIf],
   templateUrl: './sample.component.html',
   styleUrl: './sample.component.scss'
 })
-export class SampleComponent {
+export class SampleComponent implements OnInit  {
+
+   userInfoForm! : FormGroup;
+   formData : any;
+
+  constructor(private fb: FormBuilder) {
+
+    this.userInfoForm = this.fb.group({
+      name : ['',Validators.required],
+      email : ['',[Validators.email,Validators.required]],
+      password : ['',Validators.required,Validators.minLength(6)],
+      confirmPassword : ['',Validators.required,Validators.minLength(6)],
+    })
+   }
+
+
+
+  ngOnInit(): void {
+  }
+
+  userformSubmit(){
+    if(!this.userInfoForm.valid){{
+          this.userInfoForm.markAllAsTouched();
+          return;
+    }
+  }
+
+  this.formData.push(this.userInfoForm.value);
+  console.log(this.formData);
+}
 
 }
